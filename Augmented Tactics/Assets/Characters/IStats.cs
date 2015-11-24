19828f0e25624e30111e48f90764685827941b48
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-24 16:36:35 -0500

    Added the interface IStats

---
+++ Augmented Tactics/Assets/Characters/IStats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IStats  {
+
+	float HealthPoints { get; set; }
+	float Power { get;  set; }
+	float Defense { get; set; }
+	float Agility { get;  set; }
+	float Speed { get;  set; }
+	float MagicPower { get;  set; }
+	float MagicResist { get;  set; }
+	int ChanceForCriticalStrike { get; set; }
+	float Luck { get;  set; }
+	Random RandomValueGenerator { get; set; }
+	
+	IStats StatGeneration(float h, float p, float d, float a, float s, float mp, float mr, float l);
+	bool CheckStatsValues();
+	bool Equals(object obj);
+	int GetAttackStrenght(ICharacter fst, ICharacter snd);
+}
