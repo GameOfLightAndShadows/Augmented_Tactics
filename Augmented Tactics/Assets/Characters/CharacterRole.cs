d887a1b03aa02d49b0bf71da91af3ea47be03a78
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-24 16:35:19 -0500

    Added new enum files CharacterRole represents the role a character will have during the game CharacterType represents the type of character PlayerDirection represents the direction where the character is looking at

---
+++ Augmented Tactics/Assets/Characters/CharacterRole.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CharacterRole : MonoBehaviour {
+	Sniper=1,
+	Thief,
+	Warrior,
+	Wizard
+}
---
+++ Augmented Tactics/Assets/Characters/PlayerDirection.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerDirection : MonoBehaviour {
+	Right=1, 
+	Left=2,
+	Up=4,
+	Down=5
+}
