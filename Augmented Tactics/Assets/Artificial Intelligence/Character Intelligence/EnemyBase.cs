5cfb4e29d9e873778810bb0c2f4dfaa2aeeca739
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-25 00:47:03 -0500

    Updated the EnemyBase script with Notify. A later commit will remove Notify from CharacterBase and EnemyBase. Being the exact same thing, it is not optimal nor desired to have the same code at two different section of the code base. Created the scripts for character intelligence folder. These will be the same types as for Human Character folder. For this time around, there will be only a single level of intelligence for the character AI. In the future, it would be nice to have different levels.
    -NormalArcher
    -NormalAssassin
    -NormalBattleWizard
    -NormalHealer
    -NormalKnight
    -NormalSwordsman
    -NormalWizard

--- Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/EnemyBase.cs
+++ Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/EnemyBase.cs
@@ -2,11 +2,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
-
+using System.Linq;
 
 public class EnemyBase : CharacterObservable {
 	public List<CharacterObservable> HumanPlayers;
 	public ChoiceSelector ChoiceSelector;
+
+
+	//TODO:Removed abstract Notify in the CharacterObservable script and create an implementation directly in the file. (DRY)
+	public override void Notify()
+	{
+		if (isDamage || healthWasRaised)
+		{
+			//TODO: Type should be HealthManager
+			foreach (var o in ObserversList.OfType<HealthManager>())
+			{
+				o.UpdateObserver(this);
+			}
+		}
+		
+		if (Health.IsDead)
+		{
+			//TODO: Type should be GameManager
+			foreach (var o in ObserversList.OfType<GameManager>())
+			{
+				o.UpdateObserver(this);
+			}
+		}
+	}
 
 	//TODO: After choice has been made, the choices should be reset 
 	public void ResetChoiceSelector()
