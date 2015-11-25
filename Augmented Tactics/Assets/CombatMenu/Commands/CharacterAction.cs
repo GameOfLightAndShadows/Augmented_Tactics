2f0a049fa9fddc88e6da3f8022f3cd67726fa28e
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-24 23:09:25 -0500

    Added the CharacterAction abstract class to represent the commands that the user will be able to perform. This class, like others such as CharacterObservable, is used to implement an inversion of control and make the code base architecture less coupled.

---
+++ Augmented Tactics/Assets/CombatMenu/Commands/CharacterAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CharacterAction : ICharacterActionCommand {
+	public IReceiver Receiver { get; set; }
+	public bool IsExecuted { get; set; }
+	
+	protected CharacterActions(IReceiver receiver)
+	{
+		Receiver = receiver;
+		IsExecuted = false;
+	}
+	
+	public virtual void Execute(ICharacter caller, ICharacter chracterToInteractWith)
+	{
+	}
+	
+	public virtual void Execute(ICharacter caller)
+	{
+	}
+	
+	public virtual void Execute(ICharacter caller, int raiseDefense)
+	{
+	}
+}
