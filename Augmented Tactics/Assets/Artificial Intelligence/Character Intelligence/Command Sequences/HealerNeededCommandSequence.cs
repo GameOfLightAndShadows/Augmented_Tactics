638a8807ccd91749c4f975792c6967119a30ebe1
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-26 23:29:59 -0500

    Started implementing the healer needed sequence. For the next commit on the sequence, will need to implement  the game map. Otherwise will remain incomplete.

--- Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/Command Sequences/HealerNeededCommandSequence.cs
+++ Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/Command Sequences/HealerNeededCommandSequence.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+public class HealerNeededCommandSequence : DefensiveCommandSequence {
+	private List<Action> _healingActionToPerform;
+	private Dictionary<BaseEnemy, Cell> _healerData;
+	private int _healingRating=0;
+	private bool _movementNotNeeded;
+	private bool _isHealerInRange;
 
-public class HealerNeededCommandSequence : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+	public CommandSequence MakeCommandSequence()
+	{
+		return null;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void EvaluateCommandSequence()
+	{
+		if (!InDireNeedOfHealingMagic(0.5f))
+			return;
+		_healingRating += 50;
+		if (InDireNeedOfHealingMagic(0.4f))
+			_healingRating += 80;
+		if (InDireNeedOfHealingMagic(0.3f))
+			_healingRating += 120;
+		if (InDireNeedOfHealingMagic(0.2f))
+			_healingRating += 160;
+		if (InDireNeedOfHealingMagic(0.1f))
+			_healingRating += 200;
+	}
+
+
+	private bool InDireNeedOfHealingMagic(float percent)
+	{
+		var curHealth = (float)Observable.Health.CurrentHealth;
+		var maxHealth = (float)Observable.Health.MaxHealth;
+		return curHealth / maxHealth <= percent;
 	}
 }
