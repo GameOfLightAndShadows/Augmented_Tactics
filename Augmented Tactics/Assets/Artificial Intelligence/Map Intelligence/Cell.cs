5532522e9c29789e00475c51ff45cfa7a09511e4
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-27 16:58:17 -0500

    Started implementing scripts to represent the map elements for the grid(Cell) and Node which will also be used to implement an A* search to give the power to the AI to used pathfinding to find the correct path to follow to get to their goal!

---
+++ Augmented Tactics/Assets/Artificial Intelligence/Map Intelligence/Cell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour
+{
+	public Vector2 Coordinates { get; set; }
+	
+	public bool UseByCharacter;
+	
+	public virtual bool IsWalkable()
+	{
+		return true;
+	}
+	
+	public virtual float MovementCost()
+	{
+		return 0;
+	}
+}
---
+++ Augmented Tactics/Assets/Artificial Intelligence/Map Intelligence/Node.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node
+{
+	public int X;
+	public int Y;
+	public float G;
+	public float H;
+	public float MoveCost;
+	public Node Parent;
+	public Cell Cell;
+
+	public Node(int x, int y, float g, float moveCost, float h, Node parent, Cell c)
+	{
+		X = x;
+		Y = y;
+		G = g;
+		H = h;
+		MoveCost = moveCost;
+		Parent = parent;
+		Cell = c;
+	}
+
+}
