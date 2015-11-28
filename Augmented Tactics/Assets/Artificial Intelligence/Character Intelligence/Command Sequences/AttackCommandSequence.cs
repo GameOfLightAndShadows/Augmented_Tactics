9a1f10bfabcfda7f06a52e124d9b46d0d49a3577
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-27 21:57:58 -0500

    OffensiveCommandSequence no recognize anymore by project. Deleted and new class AttackCommandSequence created to replace it

---
+++ Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/Command Sequences/AttackCommandSequence.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AttackCommandSequence : CommandSequence {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
