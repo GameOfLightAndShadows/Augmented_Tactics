6b97cfc454beac79e09b359a512ee83dccb8c1fb
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-28 19:25:09 -0800

    Added the prefab to represent a cell in the map Created a script to hold all the prefab in the game that will be access via the static script instance Added the testscene to the scene folder. gamescene will be added when the prototype is completed

---
+++ Augmented Tactics/Assets/PrefabHolder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabHolder : MonoBehaviour {
+    public static PrefabHolder instance;
+    public GameObject BASE_TILE_PREFAB;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+}
