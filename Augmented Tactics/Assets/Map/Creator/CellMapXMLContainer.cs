77c572886b4ed7b07599c4562110ddd7d71aad5f
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-28 19:31:05 -0800

    Added a creator map folder to serialize the map data in an xml file and use it to correctly load the map in the game.

---
+++ Augmented Tactics/Assets/Map/Creator/CellMapXMLContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Map.Creator
+{
+    [XmlRoot("MapCollection")]
+    public class CellMapXMLContainer
+    {
+        [XmlAttribute("size")]
+        public int size;
+
+        [XmlArray("Cells")]
+        [XmlArrayItem("Cell")]
+        public List<CellXML> tiles = new List<CellXML>();
+    }
+}
---
+++ Augmented Tactics/Assets/Map/Creator/CellXML.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Map.Creator
+{
+    public class CellXML
+    {
+        [XmlAttribute("id")]
+        public int id;
+
+        [XmlAttribute("locX")]
+        public int locX;
+
+        [XmlAttribute("locY")]
+        public int locY;
+    }
+}
