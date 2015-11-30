2e1c1be4c6751f1db14f9d94fd87f20cad4cf7d1
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-29 21:51:19 -0500

    Forgot characterxmlcontainer in previous commit created script to represent core elements to represent a character as an xml element.

--- Augmented Tactics/Assets/Characters/Character Serializer/CharacterXML.cs
+++ Augmented Tactics/Assets/Characters/Character Serializer/CharacterXML.cs
@@ -1,11 +1,36 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Assets.Characters.Character_Serializer
 {
-    class CharacterXML
+    public class CharacterXML
     {
+        [XmlAttribute("Type")]
+        public CharacterType type;
+
+        [XmlAttribute("Role")]
+        public CharacterRole role;
+
+        [XmlAttribute("Health")]
+        public Health health;
+
+        [XmlAttribute("Stats")]
+        public CharacterStats stats;
+
+        [XmlAttribute("Current Position")]
+        public Cell currentPosition;
+
+        [XmlAttribute("Old Position")]
+        public Cell oldPosition;
+
+        [XmlAttribute("Direction")]
+        public PlayerDirection direction;
+
+        [XmlAttribute("Observers")]
+        public ArrayList observers;
     }
 }
---
+++ Augmented Tactics/Assets/Characters/Character Serializer/CharacterXMLContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Characters.Character_Serializer
+{
+    class CharacterXMLContainer
+    {
+    }
+}
