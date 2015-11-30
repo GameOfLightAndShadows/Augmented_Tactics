b7b0518cfb921970856d3540dbeaab3d098d3fba
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-30 09:13:21 -0500

    Added the script responsible to generate the game objects in the game. The map and the characters

---
+++ Augmented Tactics/Assets/Managers/GameObjectSpawmer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Managers
+{
+    public class GameObjectSpawmer
+    {
+        public List<CharacterObservable> Humans;
+        public List<CharacterObservable> Enemies;
+        private AStarPathfinder _pathfinder;
+        private HealthManager _healthManager;
+        private GameManager _gameManager;
+        public GameMap _map;
+        public GameObject spawmer;
+
+        public GameMap GenerateGameMap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CharacterObservable> GenerateGameCharacters()
+        {
+            var gameCharacters = new List<CharacterObservable>();
+            gameCharacters
+                .AddRange(GenerateHumans());
+            gameCharacters
+                .AddRange(GenerateEnemies());
+            return gameCharacters;
+        }
+
+        private List<CharacterObservable> GenerateHumans()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<CharacterObservable> GenerateEnemies()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
