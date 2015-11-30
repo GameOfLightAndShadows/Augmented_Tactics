e57185c50a8c5b32fa8266a3809fcfcbcf30bd30
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-29 21:54:52 -0500

    added the implementation of the game character xml document container.

--- Augmented Tactics/Assets/Characters/Character Serializer/CharacterXMLContainer.cs
+++ Augmented Tactics/Assets/Characters/Character Serializer/CharacterXMLContainer.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
+using Assets.Map.Creator;
 
 namespace Assets.Characters.Character_Serializer
 {
-    class CharacterXMLContainer
+    [XmlRoot ("Characters Collection")]
+    public class CharacterXMLContainer
     {
+        [XmlAttribute("NumberCharacter")]
+        public int number;
+
+        [XmlArray("Humans")]
+        [XmlArrayItem("Human")]
+        public List<CharacterXML> humans = new List<CharacterXML>();
+
+        [XmlArray("AI Characters")]
+        [XmlArrayItem("AI Character")]
+        public List<CharacterXML> ai = new List<CharacterXML>();
     }
 }
