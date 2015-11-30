3f832745fe94e8bdcbcc39083b5baecb7fcff99b
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-11-30 09:54:54 -0500

    Implemented the choice selector each command sequence class that is provided to the choice selector is evaluated and the command sequence is created afterwards during the Think phase. When it comes down to make the best choice available to the current situation, looks at the command sequence with the highest score and selects it. Finally ExecuteBestMove looks at each Action in the list and executes it.

--- Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/Command Sequences/CommandSequence.cs
+++ Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/Command Sequences/CommandSequence.cs
@@ -1,38 +1,42 @@
-﻿using UnityEngine;
-using System.Collections;
-using System;
+﻿using System;
 using System.Collections.Generic;
-public abstract class CommandSequence : ICommandAction {
-	#region Implemented Methods
-	public bool CanSelfMove()
-	{
-		//Will be based on coordinate of self in the game map.
-		//Implementation will have to wait. 
-		throw new NotImplementedException ();
-	}
+
+namespace Assets.Artificial_Intelligence.Character_Intelligence.Command_Sequences
+{
+    public abstract class CommandSequence : ICommandAction {
+        public int CommandSequenceScore { get; set; }      
+
+        #region Implemented Methods
+        public bool CanSelfMove()
+        {
+            //Will be based on coordinate of self in the game map.
+            //Implementation will have to wait. 
+            throw new NotImplementedException ();
+        }
 	
-	public object FindCharacter()
-	{
-		//Will be based on coordinate of self in the game map.
-		//Implementation will have to wait.
-		throw new NotImplementedException ();
-	}
+        public object FindCharacter()
+        {
+            //Will be based on coordinate of self in the game map.
+            //Implementation will have to wait.
+            throw new NotImplementedException ();
+        }
 
-	public bool IsFacingCharacter(CharacterObservable self, CharacterObservable target)
-	{
-		return Math.Abs (self.Direction - target.Direction) == 1;
-	}
+        public bool IsFacingCharacter(CharacterObservable self, CharacterObservable target)
+        {
+            return Math.Abs (self.Direction - target.Direction) == 1;
+        }
 
-	public void RotateTowardsCharacter(CharacterObservable character)
-	{
-		throw new NotImplementedException ();
-	}
-	#endregion Implemented Methods
+        public void RotateTowardsCharacter(CharacterObservable character)
+        {
+            throw new NotImplementedException ();
+        }
+        #endregion Implemented Methods
 
-	#region Abstract Methods
-	public abstract List<Action> MakeCommandSequence();
-	public abstract void MoveTowardsCharacter(CharacterObservable character);
-	public abstract void EvaluateCommandSequence();
-	#endregion Abstract Methods
+        #region Abstract Methods
+        public abstract List<Action> MakeCommandSequence();
+        public abstract void MoveTowardsCharacter(CharacterObservable character);
+        public abstract void EvaluateCommandSequence();
+        #endregion Abstract Methods
 
+    }
 }
