90bac82c620bc877b21da0ad7fa770102a8d2f0e
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-12-12 17:31:02 -0500

    Implemented the combat UI with Rect and Buttons for Attack Defend Move Rotate Skip End Turn Undo

--- Augmented Tactics/Assets/Characters/CharacterBase.cs
+++ Augmented Tactics/Assets/Characters/CharacterBase.cs
@@ -1,8 +1,11 @@
 ﻿using System.Linq;
+using UnityEngine;
 using Random = System.Random;
 
 public class CharacterBase : CharacterObservable
 {
+    public CommandManager _cmdManager;
+
     public bool CanDoExtraDamage()
     {
         if (Stats.ChanceForCriticalStrike * Stats.Luck < 50) return false;
@@ -49,5 +52,146 @@ public class CharacterBase : CharacterObservable
     // Update is called once per frame
     private void Update()
     {
+    }
+
+    public override void TurnOnGUI()
+    {
+        float buttonHeight = 50;
+        float buttonWidth = 150;
+
+        Rect buttonRect = new Rect(0, Screen.height - buttonHeight * 7, buttonWidth, buttonHeight);
+
+        //move button
+        if (GUI.Button(buttonRect, "Move"))
+        {
+            if (!_isMoving)
+            {
+                //There should be a GameMap manager or at the very least a TileManager
+                //GameManager.instance.removeTileHighlights();
+                _isMoving = true;
+                _isAttacking = false;
+                _hasPerformedAtLeastOneMove = true;
+                _isDefending = false;
+                _isRotating = false;
+                //GameManager.instance.highlightTilesAt(gridPosition, Color.blue, movementPerActionPoint, false);
+            }
+            else
+            {
+                _isMoving = false;
+                _isAttacking = false;
+                _hasPerformedAtLeastOneMove = false;
+                _isDefending = false;
+                _isRotating = false;
+                //GameManager.instance.removeTileHighlights();
+            }
+        }
+
+        //attack/heal button
+        buttonRect = new Rect(0, Screen.height - buttonHeight * 6, buttonWidth, buttonHeight);
+
+        if (GUI.Button(buttonRect, "Attack"))
+        {
+            if (!_isAttacking)
+            {
+                //GameManager.instance.removeTileHighlights();
+                _isMoving = false;
+                _isAttacking = true;
+                _hasPerformedAtLeastOneMove = true;
+                _isDefending = false;
+                _isRotating = false;
+                //GameManager.instance.highlightTilesAt(gridPosition, Color.red, attackRange);
+            }
+            else
+            {
+                _isMoving = true;
+                _isAttacking = false;
+                _hasPerformedAtLeastOneMove = true;
+                _isDefending = false;
+                _isRotating = false;
+                //GameManager.instance.removeTileHighlights();
+            }
+        }
+
+        //defend button
+        buttonRect = new Rect(0, Screen.height - buttonHeight * 5, buttonWidth, buttonHeight);
+
+        if (GUI.Button(buttonRect, "Defend"))
+        {
+            if (!_isDefending)
+            {
+                //GameManager.instance.removeTileHighlights();
+                _isMoving = false;
+                _isAttacking = false;
+                _hasPerformedAtLeastOneMove = true;
+                _isDefending = true;
+                _isRotating = false;
+                //GameManager.instance.highlightTilesAt(gridPosition, Color.red, attackRange);
+            }
+            else
+            {
+                _isMoving = false;
+                _isAttacking = false;
+                _isDefending = false;
+                _isRotating = false;
+                //GameManager.instance.removeTileHighlights();
+            }
+        }
+
+        //rotate button
+        buttonRect = new Rect(0, Screen.height - buttonHeight * 4, buttonWidth, buttonHeight);
+
+        if (GUI.Button(buttonRect, "Rotate"))
+        {
+            if (!_isRotating)
+            {
+                //GameManager.instance.removeTileHighlights();
+                _isMoving = false;
+                _isAttacking = true;
+                _hasPerformedAtLeastOneMove = false;
+                _isDefending = false;
+                _isRotating = true;
+                //GameManager.instance.highlightTilesAt(gridPosition, Color.red, attackRange);
+            }
+            else
+            {
+                _isMoving = true;
+                _isAttacking = false;
+                _hasPerformedAtLeastOneMove = true;
+                _isDefending = false;
+                _isRotating = false;
+                //GameManager.instance.removeTileHighlights();
+            }
+        }
+
+        //undo button
+        buttonRect = new Rect(0, Screen.height - buttonHeight * 3, buttonWidth, buttonHeight);
+
+        if (GUI.Button(buttonRect, "Undo"))
+        {
+            if (_hasPerformedAtLeastOneMove)
+            {
+                _cmdManager.Undo(this, _cmdManager.LastCharacterAttacked);
+                if (_cmdManager.CommandStack.Count == 0)
+                    _hasPerformedAtLeastOneMove = false;
+            }
+        }
+
+        //skip button
+        buttonRect = new Rect(0, Screen.height - buttonHeight * 2, buttonWidth, buttonHeight);
+
+        if (GUI.Button(buttonRect, "Skip"))
+        {
+            GameManager.instance.GoToNextCharacter();
+        }
+
+        //end turn button
+        buttonRect = new Rect(0, Screen.height - buttonHeight * 1, buttonWidth, buttonHeight);
+
+        if (GUI.Button(buttonRect, "End Turn"))
+        {
+            GameManager.instance.GoToNextCharacter();
+        }
+
+        base.TurnOnGUI();
     }
 }
