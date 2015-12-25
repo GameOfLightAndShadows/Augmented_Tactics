60a951b3ceb96d92b49611cdab46dd91cb7b992a
Kevin Avignon <kevin.o.avignon@gmail.com>
2015-12-25 15:36:05 -0500

    Added a new command sequence for the AI It will attack the most damageable target based on characters stats state of equipment and its role

---
+++ Augmented Tactics/Assets/Artificial Intelligence/Character Intelligence/Command Sequences/AttackMostDamageableTargetCommandSequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Artificial_Intelligence.Character_Intelligence.Command_Sequences
+{
+    public class AttackMostDamageableTargetCommandSequence: AttackCommandSequence
+    {
+        private List<Action> _mostDamageableTargetCommandSequenceAction;
+        private int _mostDamageableRating;
+        private bool _isMoveNeeded;
+        private bool _isFacingCharacter;
+        private bool _isTargetInAttackRange;
+
+
+        public override List<Action> MakeCommandSequence()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void MoveTowardsCharacter(CharacterObservable character)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EvaluateCommandSequence()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SelectTarget(CharacterObservable[] humans)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
